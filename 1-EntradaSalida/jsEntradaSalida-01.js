b28d4c4d1ef6a500eaa8f064b31315e4fec7bc4d
abreguVictoria <86858965+abreguVictoria@users.noreply.github.com>
2021-07-07 03:51:04 -0300

    Add files via upload
    comentarios en bloque y comentarios en línea

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -1,8 +1,17 @@
 /*
+autor: Abregú Mariela.
+enunciado ejercicio 01:
 Al presionar el  botón, 
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("ok");
-}
+	alert("Esto funciona de maravilla");
+	// el alert siempre va con paréntesis //
 
+	/*
+	comentario en bloque 
+	comienza con barra asteriasco 
+	y finaliza con asterisco barra.
+	son multilinea
+	*/
+}
